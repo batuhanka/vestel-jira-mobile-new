4907dfb5b9a4a768b74d91f59486fe7879768377
batuhanka <batuhankandiran@gmail.com>
2016-04-18 20:56:30 +0300

    Resolved onBackPressed problem

--- app/src/main/java/navigation/NavigationActivity.java
+++ app/src/main/java/navigation/NavigationActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -71,7 +73,8 @@ public class NavigationActivity extends AppCompatActivity implements NavigationV
         userFullNameView.setText(userFullNameText);
 
        FragmentManager fragmentManager = getSupportFragmentManager();
-       fragmentManager.beginTransaction().add(R.id.contentNav, new ActivityStreamFragment()).addToBackStack("ActivityStreamFragment").commit();
+       fragmentManager.beginTransaction().replace(R.id.contentNav, new ActivityStreamFragment()).addToBackStack("ActivityStreamFragment").commit();
+       fragmentManager.executePendingTransactions();
 
     }
 
@@ -91,41 +94,22 @@ public class NavigationActivity extends AppCompatActivity implements NavigationV
         return Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
     }
 
-    @Override
-    public void onBackPressed() {
-
-        //TODO: Onbackpress event method must be override
-        super.onBackPressed();
-//        int stackCount = fragmentManager.getBackStackEntryCount();
-//
-//        if(stackCount > 1) {
-//            fragmentManager.beginTransaction().remove(fragmentManager.getFragments().get(stackCount - 1)).commit();
-//            Log.e("BATU","COUNT : "+stackCount);
-//            Fragment latestFragment = fragmentManager.getFragments().get(stackCount - 2);
-//            Log.e("BATU", "LATEST : " + latestFragment);
-//            fragmentManager.beginTransaction().replace(R.id.contentNav, latestFragment).addToBackStack(latestFragment.getTag()).commit();
-//        }
-
-       // fragmentManager.beginTransaction().add(R.id.contentNav, new ActivityStreamFragment()).addToBackStack("ActivityStreamFragment").commit();
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id                                  = item.getItemId();
         FragmentManager fragmentManager         = getSupportFragmentManager();
-        RelativeLayout container                = (RelativeLayout) findViewById(R.id.contentNav);
 
         if (id == R.id.nav_assigned_issues) {
-            container.removeAllViews();
-            fragmentManager.beginTransaction().add(R.id.contentNav, new AssignedToMeFragment()).addToBackStack("AssignedFragment").commit();
+            fragmentManager.beginTransaction().replace(R.id.contentNav, new AssignedToMeFragment()).addToBackStack("AssignedFragment").commit();
+            fragmentManager.executePendingTransactions();
         } else if (id == R.id.nav_reported_issues) {
-            container.removeAllViews();
-            fragmentManager.beginTransaction().add(R.id.contentNav, new ReportedToMeFragment()).addToBackStack("ReportedFragment").commit();
+            fragmentManager.beginTransaction().replace(R.id.contentNav, new ReportedToMeFragment()).addToBackStack("ReportedFragment").commit();
+            fragmentManager.executePendingTransactions();
         } else if (id == R.id.nav_search_issues) {
-            container.removeAllViews();
-            fragmentManager.beginTransaction().add(R.id.contentNav, new SearchIssueFragment()).addToBackStack("SearchFragment").commit();
+            fragmentManager.beginTransaction().replace(R.id.contentNav, new SearchIssueFragment()).addToBackStack("SearchFragment").commit();
+            fragmentManager.executePendingTransactions();
         } else if (id == R.id.nav_projects) {
 
         } else if (id == R.id.nav_about) {
