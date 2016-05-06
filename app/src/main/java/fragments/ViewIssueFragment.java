64bd33e8210be5f63bc7efe1f7782b0d04694156
batuhanka <batuhankandiran@gmail.com>
2016-05-07 00:34:04 +0300

    Resolved some warnings.

--- app/src/main/java/fragments/ViewIssueFragment.java
+++ app/src/main/java/fragments/ViewIssueFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -14,17 +13,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.List;
 
 import adapter.CommentAdapter;
-import adapter.CommentModel;
 import adapter.ImageLoader;
 import adapter.ViewIssueModel;
 import navigation.NavigationActivity;
